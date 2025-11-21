edaa20be2946359ee25776a0c941c15fc3f3974d
munstr001 <derdad506@gmail.com>
2025-11-22 03:05:21 +0500

    Rename README automation files

---
+++ scripts/daily_random_commits.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+ROOT_DIR=$(git rev-parse --show-toplevel)
+FILE="$ROOT_DIR/README.md"
+
+ensure_trailing_newline() {
+  if [[ -s "$FILE" && $(tail -c1 "$FILE") != $'\n' ]]; then
+    printf '\n' >> "$FILE"
+  fi
+}
+
+append_entry() {
+  local stamp rand
+  stamp=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
+  rand=$(openssl rand -hex 4)
+  printf "- %s | token: %s\n" "$stamp" "$rand" >> "$FILE"
+}
+
+ensure_trailing_newline
+
+if [[ $(tail -n1 "$FILE") != "" ]]; then
+  printf '\n' >> "$FILE"
+fi
+
+for i in 1 2 3; do
+  append_entry
+  git add "$FILE"
+  git commit -m "chore: random README update ($i/3)"
+  sleep 1
+done
